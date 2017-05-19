3107922cdb4670983d7bd746b2b24fdec6e611f8
Nick Dobson <nick.dobson@ge.com>
2017-05-18 23:39:25 -0500

    Version 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'nick.dobson@me.com'
 license 'Apache-2.0'
 description 'Installs/Configures chef_jenkins'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.3'
+version '0.2.0'
 chef_version '>= 12.5' if respond_to?(:chef_version)
 issues_url 'https://github.com/ndobson/chef_jenkins/issues'
 source_url 'https://github.com/ndobson/chef_jenkins'
